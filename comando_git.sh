5e8c6a6dba4e3c487a517130d46d7def318ae8dc
Vitor Andrade <vitor.andrade@localhost.localdomain>
2025-03-12 18:39:23 -0300

    Commit inicial do projeto PI Mobile completo

---
+++ comando_git.sh
@@ -0,0 +1,11 @@
+# Verifique qual remote está configurado
+git remote -v
+
+# Remova o remote atual
+git remote remove origin
+
+# Adicione o remote correto
+git remote add origin https://github.com/Vitor1s/Pi-Mobile-Agendamentos.git
+
+# Verifique se está correto agora
+git remote -v
